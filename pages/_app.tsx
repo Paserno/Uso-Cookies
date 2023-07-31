dbd1f413ba8d7b7f9fd39ebeadc7a9dc299f8a08
Paserno <paserno.x@gmail.com>
2023-07-30 22:25:00 -0400

    Instalacion de material UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { darkTheme, lightTheme, customTheme } from '../theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={ darkTheme }>
+      <CssBaseline />
+      <Component {...pageProps} />
+
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
