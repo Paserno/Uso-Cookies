fd6968f197cc75a5ce66c4a98fe5f152a0a6e6be
Paserno <paserno.x@gmail.com>
2023-07-31 19:13:45 -0400

    Establecer thema usando las cookie

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,64 @@
+import { useEffect, useState } from 'react';
+import type { AppContext, AppProps } from 'next/app'
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ThemeProvider, CssBaseline } from '@mui/material';
+import { ThemeProvider, CssBaseline, Theme } from '@mui/material';
 import { darkTheme, lightTheme, customTheme } from '../theme';
+import Cookies from 'js-cookie';
 
-function MyApp({ Component, pageProps }: AppProps) {
+
+interface Props extends AppProps {
+  theme: string;
+}
+
+function MyApp({ Component, pageProps, theme = 'dark' }: Props) {
+
+  // console.log({theme});
+  
+  const [currentTheme, setCurrentTheme] = useState(lightTheme);
+
+  useEffect(() => {
+    const cookieTheme = Cookies.get('theme') || 'light';
+    const selectedTheme = cookieTheme === 'light' 
+        ? lightTheme
+        : (cookieTheme === 'dark') 
+          ? darkTheme
+          : customTheme
+    
+    setCurrentTheme( selectedTheme );
+  }, [])
+  
+  
+
+
   return (
-    <ThemeProvider theme={ darkTheme }>
+    <ThemeProvider theme={ currentTheme }>
       <CssBaseline />
       <Component {...pageProps} />
 
     </ThemeProvider>
   )
 }
+
+
+// MyApp.getInitialProps = async( appContext: AppContext) => {
+
+//   const { theme } = appContext.ctx.req ? ( appContext.ctx.req as any).cookies : { theme: 'light' }; 
+
+//   // console.log('getInitialProps: ',cookies);
+//   const validThemes = ['light', 'dark', 'custom'];
+
+
+//   return {
+//     theme: validThemes.includes(theme) ? theme : 'dark',
+//   }
+
+// }
+
 
 export default MyApp;
--- pages/theme-changer.tsx
+++ pages/theme-changer.tsx
@@ -1,24 +1,41 @@
-import { useState, ChangeEvent, FC } from 'react';
+import { useState, ChangeEvent, FC, useEffect } from 'react';
 import { GetServerSideProps } from 'next';
 
-import { Card, CardContent, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from '@mui/material';
-import { Layout } from '../components/layout';
+import { Card, CardContent, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, Button } from '@mui/material';
 import Cookies from 'js-cookie';
+import axios from 'axios';
 
-const ThemeChangerPage: FC = (props) => {
+import { Layout } from '../components/layout';
 
-    console.log({ props })
+interface Props {
+    theme: string;
+}
 
-    const [currentTheme, setCurrentTheme] = useState('light');
+const ThemeChangerPage: FC<Props> = ({ theme }) => {
+
+    // console.log({ theme })
+
+    const [currentTheme, setCurrentTheme] = useState(theme);
 
     const onThemeChange = (event: ChangeEvent<HTMLInputElement>) => {
         const selectedTheme = event.target.value;
-        
-        console.log(selectedTheme)
-        setCurrentTheme( selectedTheme );
 
-        Cookies.set('theme', selectedTheme );
+        console.log(selectedTheme)
+        setCurrentTheme(selectedTheme);
+
+        Cookies.set('theme', selectedTheme);
     }
+
+    const onClick = async () => {
+        const { data } = await axios.get('/api/hello');
+        console.log({ data })
+    }
+
+    useEffect(() => {
+        console.log('Cookies:', Cookies.get('theme'));
+
+    }, [])
+
 
     return (
         <Layout>
@@ -27,14 +44,20 @@ const ThemeChangerPage: FC = (props) => {
                     <FormControl>
                         <FormLabel>Tema</FormLabel>
                         <RadioGroup
-                            value={ currentTheme }
-                            onChange={ onThemeChange }
+                            value={currentTheme}
+                            onChange={onThemeChange}
                         >
                             <FormControlLabel value='light' control={<Radio />} label="Light" />
                             <FormControlLabel value='dark' control={<Radio />} label="Dark" />
                             <FormControlLabel value='custom' control={<Radio />} label="Custom" />
                         </RadioGroup>
                     </FormControl>
+
+                    <Button
+                        onClick={onClick}
+                    >
+                        Solicitud
+                    </Button>
                 </CardContent>
             </Card>
         </Layout>
@@ -46,13 +69,14 @@ const ThemeChangerPage: FC = (props) => {
 // - Only if you need to pre-render a page whose data must be fetched at request time
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-    
-    const { theme = 'light', name = 'No Name'} = req.cookies;
 
+    const { theme = 'light', name = 'No Name' } = req.cookies;
+
+    const validThemes = ['light', 'dark', 'custom'];
 
     return {
         props: {
-            theme,
+            theme: validThemes.includes(theme) ? theme : 'dark',
             name,
         }
     }
